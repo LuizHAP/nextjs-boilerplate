1dc5b1a66861e9c208e7ecb4d01e3f5bf0685612
Luiz Pansarini <lpansarini@gmail.com>
2021-10-11 10:48:26 -0300

    finalizando boilerplate

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,1 +1,2 @@
 import '@testing-library/jest-dom' //matchers do Jest
+import 'jest-styled-components'
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom', //ambiente para teste
   testPathIgnorePatterns: ['/node_modules/', '/.next/'], //ignora esses arquivos
   collectCoverage: true, // retorna o coverage
-  collectCoverageFrom: ['src/**/*.ts(x)'], //de onde pega o coverage
+  collectCoverageFrom: ['components/**/*.ts(x)'], //de onde pega o coverage
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'] // irá executar antes dos testes
 }
